739c51cb6dbc6ab95c8e4f20b172750db7f53d1d
Ross Butler <ross.butler@gocity.com>
2022-05-14 13:02:18 +0100

    Make Connectivity configurable

--- code/ConnectivitySubscription.swift
+++ code/ConnectivitySubscription.swift
@@ -12,10 +12,11 @@ import Foundation
 import OpenCombine
 
 class ConnectivitySubscription<S: Subscriber>: Subscription where S.Input == Connectivity, S.Failure == Never {
-    private let connectivity = Connectivity()
+    private let connectivity: Connectivity
     private var subscriber: S?
 
-    init(subscriber: S) {
+    init(configuration: ConnectivityConfiguration, subscriber: S) {
+        connectivity = Connectivity(configuration: configuration)
         self.subscriber = subscriber
         startNotifier(with: subscriber)
     }
